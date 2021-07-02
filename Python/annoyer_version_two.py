fe2387277ffd057e430122b72b0f3905e1a5cb50
Eashan-EM <86050560+Eashan-EM@users.noreply.github.com>
2021-07-02 22:59:53 +0530

    Create annoyer_version_two.py

---
+++ Python/annoyer_version_two.py
@@ -0,0 +1,12 @@
+inp = input("Enter string: ")
+#Takes a string inout
+times = int(input("Enter times: "))
+#Takes the number of times to print
+i = 0
+#Variable i inistalised by 0
+while i!=times:
+	#While i is not equal to times
+	print(inp)
+	#Prints the input string
+	i += 1
+	#Variable i incremented by 1
