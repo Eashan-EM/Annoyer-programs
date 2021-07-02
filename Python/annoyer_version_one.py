79c3b23c6565cdaf3f35f93eddf948f553bc3759
Eashan-EM <86050560+Eashan-EM@users.noreply.github.com>
2021-07-02 22:53:45 +0530

    Create annoyer_version_one.py

---
+++ Python/annoyer_version_one.py
@@ -0,0 +1,14 @@
+from os import system
+#Imports system from os
+inp = input("Enter text: ")
+#Takes a input string
+times = int(input("How many times: "))
+#Takes the number of times to print
+_ = system("clear")
+#Clears the screen
+while times!=0:
+	#While times is not equal to 0
+	print(inp)
+	#Print the input string
+	times -= 1
+	#Times variable decrease by 1
