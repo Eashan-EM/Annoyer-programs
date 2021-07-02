62c701f141522330bacdd28a2c437b069b3ecf7d
Eashan-EM <86050560+Eashan-EM@users.noreply.github.com>
2021-07-02 23:05:58 +0530

    Update annoyer_version_two.py

--- Python/annoyer_version_two.py
+++ Python/annoyer_version_two.py
@@ -3,7 +3,7 @@ inp = input("Enter string: ")
 times = int(input("Enter times: "))
 #Takes the number of times to print
 i = 0
-#Variable i inistalised by 0
+#Variable i initialised by 0
 while i!=times:
 	#While i is not equal to times
 	print(inp)
